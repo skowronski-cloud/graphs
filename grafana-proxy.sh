0e58219507fc12f20b72be6f007a75ea971dcb5e
Daniel Skowroński <daniel@dsinf.net>
2021-12-01 00:57:00 +0100

    reorg files, update script

--- grafana-proxy.sh
+++ grafana-proxy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 BASEURL=http://$1:3000/render/d-solo
 TOKEN=$2
+mkdir graphs
+set -x
+function download() {
+	curl -H 'Authorization: Bearer '$TOKEN $BASEURL/$2 -o $1.png.swp
+	mv $1.png.swp graphs/$1.png
+	chmod 0744 graphs/$1.png
+}
 
 # C10 inside
 ## Study
 ### gauges
-curl -H 'Authorization: Bearer '$TOKEN $BASEURL'/ZvAMlXp7k/c10-inside?panelId=8&from=now-6h&to=now&width=720&height=200' -o c10_inside_study_temp.png.swp
-mv c10_inside_study_temp.png.swp c10_inside_study_temp.png
-curl -H 'Authorization: Bearer '$TOKEN $BASEURL'/ZvAMlXp7k/c10-inside?panelId=6&from=now-6h&to=now&width=720&height=200' -o c10_inside_study_co2.png.swp
-mv c10_inside_study_co2.png.swp c10_inside_study_co2.png
-curl -H 'Authorization: Bearer '$TOKEN $BASEURL'/ZvAMlXp7k/c10-inside?panelId=7&from=now-6h&to=now&width=720&height=200' -o c10_inside_study_humid.png.swp
-mv c10_inside_study_humid.png.swp c10_inside_study_humid.png
+download c10_inside_study_temp   'ZvAMlXp7k/c10-inside?panelId=8&from=now-6h&to=now&width=720&height=200' 
+download c10_inside_study_co2    'ZvAMlXp7k/c10-inside?panelId=6&from=now-6h&to=now&width=720&height=200'
+download c10_inside_study_humid  'ZvAMlXp7k/c10-inside?panelId=7&from=now-6h&to=now&width=720&height=200' 
 ### graphs
-curl -H 'Authorization: Bearer '$TOKEN $BASEURL'/ZvAMlXp7k/c10-inside?panelId=2&from=now-6h&to=now&width=720&height=480' -o c10_inside_study_graph1.png.swp
-mv c10_inside_study_graph1.png.swp c10_inside_study_graph1.png
-curl -H 'Authorization: Bearer '$TOKEN $BASEURL'/ZvAMlXp7k/c10-inside?panelId=3&from=now-6h&to=now&width=720&height=480' -o c10_inside_study_graph2.png.swp
-mv c10_inside_study_graph2.png.swp c10_inside_study_graph2.png
+download c10_inside_study_graph1 'ZvAMlXp7k/c10-inside?panelId=2&from=now-6h&to=now&width=720&height=480'
+download c10_inside_study_graph2 'ZvAMlXp7k/c10-inside?panelId=3&from=now-6h&to=now&width=720&height=480'
