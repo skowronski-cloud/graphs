738d3975d6d66888a53ea4b2f705572bf1a7e1a8
Daniel Skowroński <daniel@dsinf.net>
2021-12-03 01:00:40 +0100

    add bedroom measurements, rename few graphs

--- grafana-proxy.sh
+++ grafana-proxy.sh
@@ -16,8 +16,16 @@ download c10_study_temp      'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=n
 download c10_study_co2       'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=200&panelId=6'
 download c10_study_humid     'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=200&panelId=7' 
 ### graphs
-download c10_study_graph1    'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=480&panelId=2'
-download c10_study_graph2    'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=480&panelId=3'
+download c10_study_env       'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=480&panelId=2'
+download c10_study_air       'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=480&panelId=3'
+## Study
+### stats
+download c10_bedroom_temp    'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=200&panelId=23' 
+download c10_bedroom_co2     'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=200&panelId=24'
+download c10_bedroom_humid   'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=200&panelId=25' 
+### graphs
+download c10_bedroom_env     'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=480&panelId=26'
+download c10_bedroom_air     'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=480&panelId=27'
 ## Outside
 ### status
 download c10_outside_temp    'zBlCiep7z/graphs-skowronski-cloud?from=now-6h&to=now&width=720&height=200&panelId=13' 
